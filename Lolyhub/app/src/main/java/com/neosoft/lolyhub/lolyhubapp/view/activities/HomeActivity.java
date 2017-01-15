c83542ada76782077ff6d5f0945075fdab7c8922
Ultimate <rajendra.pawar@neosofttech.com>
2017-01-15 19:26:37 +0530

    navigation drawer for search view

--- Lolyhub/app/src/main/java/com/neosoft/lolyhub/lolyhubapp/view/activities/HomeActivity.java
+++ Lolyhub/app/src/main/java/com/neosoft/lolyhub/lolyhubapp/view/activities/HomeActivity.java
@@ -2,11 +2,17 @@ package com.neosoft.lolyhub.lolyhubapp.view.activities;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -24,7 +30,7 @@ import com.neosoft.lolyhub.lolyhubapp.rest.model.Result;
  * Created by neosoft on 29/12/16.
  */
 
-public class HomeActivity extends AppCompatActivity implements NetworkReceiver{
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener,NetworkReceiver{
     public static String TAG="HomeActivity";
     private RecyclerView mRecyclerView;
     private CardAdapter mCardAdapter;
@@ -37,43 +43,23 @@ public class HomeActivity extends AppCompatActivity implements NetworkReceiver{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         myOnClickListener=new MyOnClickListener(this);
        // FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         //Initializing the tablayout
         //Initializing the tablayout
-        mTabLayout = (TabLayout) findViewById(R.id.tabLayout);
-
-        //Adding the tabs using addTab() method
-        mTabLayout.addTab(mTabLayout.newTab().setText("Cart "));
-        mTabLayout.addTab(mTabLayout.newTab().setText("Wallet"));
-        mTabLayout.addTab(mTabLayout.newTab().setText("Search"));
-        mTabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
-
+           setUpTabLayout();
         //Initializing viewPager
         viewPager = (ViewPager) findViewById(R.id.pager);
-
         //Creating our pager adapter
         Pager adapter = new Pager(getSupportFragmentManager(), mTabLayout.getTabCount());
-
         //Adding adapter to pager
         viewPager.setAdapter(adapter);
-
         //Adding onTabSelectedListener to swipe views
-        mTabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                viewPager.setCurrentItem(tab.getPosition());
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-
-            }
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
-            }
-        });
+            //setup navigation drawer
+        setUpNavigationView(toolbar);
 
        // initViews();
        // initAdapter();
@@ -91,11 +77,56 @@ public class HomeActivity extends AppCompatActivity implements NetworkReceiver{
         mCardAdapter=new CardAdapter(this);
         mRecyclerView.setAdapter(mCardAdapter);
     }*/
+    private void setUpTabLayout(){
+        mTabLayout = (TabLayout) findViewById(R.id.tabLayout);
+
+        //Adding the tabs using addTab() method
+        mTabLayout.addTab(mTabLayout.newTab().setText("Cart "));
+        mTabLayout.addTab(mTabLayout.newTab().setText("Wallet"));
+        mTabLayout.addTab(mTabLayout.newTab().setText("Search"));
+        mTabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+        mTabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+    private void setUpNavigationView(Toolbar toolbar){
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -108,9 +139,30 @@ public class HomeActivity extends AppCompatActivity implements NetworkReceiver{
         if (id == R.id.action_settings) {
             return true;
         }
+
         return super.onOptionsItemSelected(item);
     }
 
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
     @Override
     public <T> void onResponse(T obj, int tag) {
         Countries countries;
@@ -134,6 +186,7 @@ public class HomeActivity extends AppCompatActivity implements NetworkReceiver{
         Toast.makeText(this, "Exception"+msg, Toast.LENGTH_SHORT).show();
 
     }
+
     public class MyOnClickListener implements View.OnClickListener {
 
         private final Context context;
